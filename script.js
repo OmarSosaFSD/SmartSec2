2164d2eef4ecc2f19646dc67924d5bb4fcb26c5c
DevOS <121842302+OmarSosaFSD@users.noreply.github.com>
2023-04-15 09:43:13 -0300

    actualizacion js
    borre los comentarios. el codigo funciona

--- script.js
+++ script.js
@@ -13,39 +13,3 @@ function topFunction(){
     document.documentElement.scrollTop=0;
 }
 
-//***NO ME FUNCIONO ESTE METODO***
-
-/*const openFacebook=document.querySelector('#face')
-const openInstagram=document.querySelector('#insta')
-const openTwitter=document.querySelector('#tw')
-const openWasap=document.querySelectorAll('.info')*/
-
-/*const listSocial={
-    facebook:'https://www.facebook.com/SmartSecOK',
-    instagram:'https://www.instagram.com/smart_sec_ok/',
-    twitter:'https:www.twitter.com',
-    whatsapp:'https://api.whatsapp.com/send?phone=5491125153562'
-}
-
-openFacebook.addEventListener('click',()=>{
-    window.open(listSocial.facebook,'blank')
-})
-openInstagram.addEventListener('click',()=>{
-    window.open(listSocial.instagram,'blank')
-})
-openTwitter.addEventListener('click',()=>{
-    window.open(listSocial.twitter,'blank')
-})
-openWasap.addEventListener('click',()=>{
-    window.open(listSocial.whatsapp,'blank')
-})*/
-//----------
-
-//***NI ESTE OTRO METODO.***
-/*function openNewTab(){
-    window.open('https://www.facebook.com/SmartSecOK','_blank')
- }
- openFacebook.addEventListener("click",openNewTab)
-*/
-
-//***nada de esto me funciono, asi que le di funcionalidad a los botones solo con html***
